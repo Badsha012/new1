19c4cd879780e9f9435f8a81e0924a7a36dbb5e1
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-31 20:52:25 +0600

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int addNumber(int a, int b){
+    return a+b;
+
+}
+int main()
+{
+    int result=addNumber(5,10);\
+    cout<<"sum="<<result<<endl;
+    return 0;
+}
